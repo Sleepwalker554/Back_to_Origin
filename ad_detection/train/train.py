3d9b85d515a48b4322c17e4eaf24e242fec83f61
Sleepwalker554 <sunluqi581@gmail.com>
2026-03-17 20:47:33 -0400

    a

--- ad_detection/train/train.py
+++ ad_detection/train/train.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from pathlib import Path
 from tqdm import tqdm
-from config import LEARNING_RATE, MAX_EPOCHS, WEIGHT_DECAY, XLSR_DIM_HIDDEN, XLSR_DROPOUT, XLSR_DIM_INPUT
+from config import LEARNING_RATE, MAX_EPOCHS, WEIGHT_DECAY, XLSR_DIM_HIDDEN, XLSR_DROPOUT, XLSR_DIM_INPUT, ETA_MIN
 from model import AD_XLSR_Model
 
 def train_one_epoch(model, train_loader, optimizer, device, epoch=None, class_weights=None):
@@ -177,7 +177,7 @@ def train(seed, train_loader, val_loader, output_dir, device, xlsr=True, class_w
     optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
 
     # Cosine annealing learning rate scheduler
-    scheduler = CosineAnnealingLR(optimizer, T_max=MAX_EPOCHS, eta_min=1e-6)
+    scheduler = CosineAnnealingLR(optimizer, T_max=MAX_EPOCHS, eta_min=ETA_MIN)
 
     # Training history
     train_losses = []
